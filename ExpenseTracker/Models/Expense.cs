0011a2c13a427facb8f639575c8382e798bff23c
DouaaDaoudi <daoudi_douaa@hotmail.com>
2025-07-14 14:09:07 -0400

    Add project files.

---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public string? Notes { get; set; }
+    }
+}
