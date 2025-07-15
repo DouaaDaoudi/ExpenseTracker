c2818796a35d849d0634a5c61c212d0d3eab1848
DouaaDaoudi <daoudi_douaa@hotmail.com>
2025-07-15 12:02:55 -0400

    update 2

--- ExpenseTracker/Models/Expense.cs
+++ ExpenseTracker/Models/Expense.cs
@@ -4,11 +4,11 @@ namespace ExpenseTracker.Models
 {
     public class Expense
     {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
+        public int Id { get; set; } 
+        public DateTime Date { get; set; } 
         public string Category { get; set; } = string.Empty;
-        public decimal Amount { get; set; }
+        public decimal Amount { get; set; } 
 
-          public string? Notes { get; set; }
+        public string? Notes { get; set; } 
     }
 }
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+
 //Connection To SQL Database 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
